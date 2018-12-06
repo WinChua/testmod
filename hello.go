a90a32e424555210d487223cbe230df29b36a39d
WinChua <winchua@foxmail.com>
2018-12-06 21:05:18 +0800

    Version 2

--- hello.go
+++ hello.go
@@ -1,10 +1,26 @@
 package testmod
 
-import "fmt"
+import (
+    "fmt"
+    "errors"
+)
 
-func Hi(name string) string {
-    return fmt.Sprintf("Hi, %s!", name)
+
+func Hi(name, lang string) (string, error) {
+    switch lang {
+    case "en":
+        return fmt.Sprintf("Hi, %s!", name), nil
+    case "pt":
+        return fmt.Sprintf("Oi, %s!", name), nil
+    case "es":
+        return fmt.Sprintf("Hola, %s!", name), nil
+    case "fr":
+        return fmt.Sprintf("Bonjour, %s!", name), nil
+    default:
+        return "", errors.New("unknown language")
+    }
 }
+
 
 func Hello(name string) string {
     return fmt.Sprintf("Hello, %s", name)
