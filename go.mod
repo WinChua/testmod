df1f3fe8e560a06cebc8d5a79bcdd31a24b6b85e
WinChua <winchua@foxmail.com>
2018-12-06 20:41:32 +0800

    Firstcommit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/WinChua/testmod
---
+++ hello.go
@@ -0,0 +1,11 @@
+package testmod
+
+import "fmt"
+
+func Hi(name string) string {
+    return fmt.Sprintf("Hi, %s", name)
+}
+
+func Hello(name string) string {
+    return fmt.Sprintf("Hello, %s", name)
+}
